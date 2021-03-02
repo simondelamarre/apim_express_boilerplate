89fa987c680e1d77e5ea12caa02ce69d7b0bba4d
simondelamarre <landscape.viewer@gmail.com>
2021-03-02 12:56:32 +0100

    middle ware examples updateds

--- src/middleware/bige-middleware.ts
+++ src/middleware/bige-middleware.ts
@@ -1,10 +1,15 @@
+/* eslint-disable no-mixed-operators */
 import {
   RequestHandler, Request, Response, NextFunction
 } from 'express';
 
+const JWT = require('jsonwebtoken');
+
 interface HandlerOptions {
-  scopes?: string[]
-  rights?: string[]
+  keyName: string;
+  scopes?: string[];
+  rights?: string[];
+  operator?: string;
 };
 
 /**
@@ -16,12 +21,66 @@ interface HandlerOptions {
 export const bigeMiddleware = (
   handler: RequestHandler,
   options?: HandlerOptions,
-): RequestHandler => async (req: Request, res: Response, next: NextFunction) => {
-  // eslint-disable-next-line no-console
-  console.log('bige middleware ', options);
+): RequestHandler => async (request: Request, res: Response, next: NextFunction) => {
   // TODO check and get setups from request headers bige-api-key bige-apim-key bige-public-key
-  return handler(req, res, next);
-  // next();
+  try {
+    const token = request.headers[options.keyName];
+    const decrypt = JWT.verify(token, process.env.BIGE_SECRET);
+    if (!options.operator || options.operator === 'matchAll') {
+      if (options.scopes) {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const scope of options.scopes) {
+          if (!decrypt.scopes.includes(scope)) {
+            throw new Error(`unauthorized by request access scope ${scope}`);
+          }
+        }
+      }
+      if (options.rights) {
+        // eslint-disable-next-line no-restricted-syntax
+        for (const right of options.scopes) {
+          if (!decrypt.rights.includes(right)) {
+            throw new Error(`unauthorized by request access rights ${right}`);
+          }
+        }
+      }
+    } else if (options.operator === 'oneOf') {
+      // eslint-disable-next-line max-len
+      if (
+        options.scopes && !decrypt.scopes
+        || !decrypt.scopes.some((scope: any) => options.scopes.includes(scope))
+      ) {
+        throw new Error(`unauthorized by request access scopes ${options.scopes}`);
+      }
+      // eslint-disable-next-line max-len
+      if (
+        options.rights && !decrypt.rights
+        || !decrypt.rights.some((right: any) => options.rights.includes(right))
+      ) {
+        throw new Error(`unauthorized by request access scopes ${options.scopes}`);
+      }
+    }
+    if (decrypt) {
+      // eslint-disable-next-line default-case
+      switch (options.keyName) {
+        case 'bige-api-key':
+          request.params.buser = decrypt;
+          break;
+        case 'bige-app-key':
+          request.params.bapp = decrypt;
+          break;
+        case 'bige-public-key':
+          request.params.bpub = decrypt;
+          break;
+        case 'bige-apim-key':
+          request.params.bapi = decrypt;
+          break;
+      }
+      return handler(request, res, next);
+    }
+    throw new Error(`UNAUTHORIZED - by ${options.keyName} authorization header`);
+  } catch (err) {
+    throw new Error(`UNAUTHORIZED - by bige keyName ${options.keyName} key ${request.headers[options.keyName]}`);
+  }
 };
 
 export default bigeMiddleware;
--- src/routes.ts
+++ src/routes.ts
@@ -11,10 +11,34 @@ const swaggerUiOptions = {
 const router = Router();
 
 // Book routes
-router.post('/book/add', bigeMiddleware(BookController.add, { scopes: ['user'] }));
-router.get('/book/all', bigeMiddleware(BookController.all, { scopes: ['user'] }));
-router.get('/book/:id', bigeMiddleware(BookController.getId, { scopes: ['user'] }));
-router.get('/book/search', bigeMiddleware(BookController.search, { scopes: ['user'] }));
+router.post('/book/add', bigeMiddleware(BookController.add, {
+  keyName: 'bige-api-key', // only connected user
+  scopes: ['user', 'admin'], // scoped user and admin
+  rights: ['ADMIN'], // with ADMIN access right from APIM
+  operator: 'matchAll' // will match scopes user and admin and match rights as ADMIN.
+}));
+router.get('/book/all', bigeMiddleware(BookController.all, {
+  keyName: 'bige-api-key', // only connected user
+  scopes: ['user', 'admin'], // scoped user or admin
+  operator: 'oneOf' // operator oneOf user or admmin scope
+}));
+router.get('/book/:id', bigeMiddleware(BookController.getId, {
+  keyName: 'bige-apim-key' // Only from authorized apps or APIs
+}));
+// Duplicate Middleware example to check app access and user logged access
+router.get('/book/search',
+  bigeMiddleware(
+    bigeMiddleware(
+      BookController.search,
+      {
+        keyName: 'bige-public-key' // Only check uniq apikey request authorization from APIM
+      }
+    ),
+    {
+      keyName: 'bige-api-key' // check user logged
+    }
+  ));
+
 // eslint-disable-next-line no-console
 console.log('apiSpec ', apiSpec);
 router.get('/openapi', (req, res) => {
